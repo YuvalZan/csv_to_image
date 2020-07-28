63ef96379dbe33775db402ed5e751f9ea0c0ab67
YZ <YZ@localhost>
2020-07-28 16:46:16 +0300

    Added configurable maximum

--- csv_to_image.py
+++ csv_to_image.py
@@ -10,6 +10,7 @@ import pandas as pd
 MAX_DIFF = 5.0
 # MAX_DIFF = 2000
 COLOR_MAP = 'RdYlGn'
+VALID_IMAGE_SUFFIXES = ('.png', '.jpg')
 
 log = logging.getLogger(__name__)
 
@@ -23,8 +24,7 @@ def read_csv(file_path, chunk_size=None):
         log.info('Reading file in chunks of {}'.format(chunk_size))
     return df
 
-def convert_to_diff(df, minimum=None):
-    minimum = minimum if minimum is not None else get_minimum(df)
+def convert_to_diff(df, minimum):
     df -= minimum
     # Remove negative values
     df[df < 0] = 0
@@ -36,20 +36,18 @@ def get_minimum(df):
     """
     return df[df > 0].min(numeric_only=True).min()
 
-def normalize_df(df):
-    mean = df.mean()
-    log.info('Normalizing table by mean {}'.format(mean.mean()))
-    return (df - mean / df.std())
-
-def df_to_image(df, image_path, minimum=None, is_normalize=False, **kwargs):
+def df_to_image(df, image_path, minimum=None, maximum=None):
+    minimum = minimum if minimum is not None else get_minimum(df)
     df = convert_to_diff(df, minimum=minimum)
-    if is_normalize:
-        df = normalize_df(df)
-    log.info('Writing image to {}'.format(image_path))
-    # imsave(image_path, df, cmap=COLOR_MAP, vmin=0, vmax=MAX_DIFF)
-    imsave(image_path, df, cmap=COLOR_MAP, **kwargs)
+    if maximum is not None:
+        # Convert the maximum to "diff"
+        maximum -= minimum
+    else:
+        maximum = df.max().max()
+    log.info('Writing image to {} with maximum as {}'.format(image_path, maximum))
+    imsave(image_path, df, cmap=COLOR_MAP, vmin=0, vmax=maximum)
 
-def imagizer(file_path, image_path, is_normalize=False, chunk_size=None, minimum=None):
+def imagizer(file_path, image_path, chunk_size=None, minimum=None):
     """
     Turns a csv file into an image or multiple images
     """
@@ -57,7 +55,7 @@ def imagizer(file_path, image_path, is_normalize=False, chunk_size=None, minimum
     if minimum is not None:
         log.info('Forced minimum value {}'.format(minimum))
     if chunk_size is None:
-        df_to_image(df, image_path, minimum=minimum, is_normalize=args.normalize)
+        df_to_image(df, image_path, minimum=minimum)
     else:
         if minimum is None:
             minimum = sys.maxsize
@@ -70,12 +68,10 @@ def imagizer(file_path, image_path, is_normalize=False, chunk_size=None, minimum
                 minimum = min(minimum, get_minimum(chunk))
             maximum = max(maximum, chunk.max().max())
         log.info('Minimum value is {} and maximum is {}'.format(minimum, maximum))
-        # Use "Diff" maximum
-        maximum -= minimum
         df = read_csv(file_path, chunk_size)
         for i, chunk in enumerate(df):
             image_chunk_path = image_path.parent / '{}_{:02}{}'.format(image_path.stem, i, image_path.suffix)
-            df_to_image(chunk, image_chunk_path, minimum=minimum, is_normalize=is_normalize, vmin=0, vmax=maximum)
+            df_to_image(chunk, image_chunk_path, minimum=minimum, maximum=maximum)
 
 def verify_args(args):
     if not args.file_path.is_file():
@@ -86,6 +82,8 @@ def verify_args(args):
     if args.log_path:
         if not args.log_path.parent.is_dir():
             raise ValueError("Invalid log_path! Must be a valid file path")
+    if args.image_path and args.image_path.suffix not in VALID_IMAGE_SUFFIXES:
+        raise ValueError("Invalid image_path! Must be in {}".format(VALID_IMAGE_SUFFIXES))
 
 def init_logging(log_path=None):
     logging.basicConfig(filename=log_path, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -96,9 +94,7 @@ def main(args):
     init_logging(args.log_path)
     file_path = args.file_path
     image_path = args.image_path if args.image_path else file_path.parent / (file_path.stem + '.png')
-    imagizer(file_path, image_path, is_normalize=args.normalize, chunk_size=args.chunk_size, minimum=args.minimum)
-    # df = read_csv(file_path)
-    # df_to_image(df, image_path, args.normalize)
+    imagizer(file_path, image_path, chunk_size=args.chunk_size, minimum=args.minimum)
 
 
 if __name__ == '__main__':
@@ -107,7 +103,6 @@ if __name__ == '__main__':
     parser.add_argument('--image-path', '-o', type=Path, help='An optional path to the resulting image. If omitted will generate a path from the input path with a different suffix')
     parser.add_argument('--chunk-size', '-c', type=int, help='Separate the resulting image to chunks')
     parser.add_argument('--minimum', '-m', type=int, help='Set a hard minimum value and zero all values below it')
-    parser.add_argument('--normalize', '-n',action='store_true', help='Normalize by mean before converting to image')
     parser.add_argument('--log-path', '-l', type=Path, help='Save the log to a file instead of stdout')
     try:
         args = parser.parse_args()
